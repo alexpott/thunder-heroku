4e92b67a88eee5e25c84c3df0f6d6325fa23901d
Alex Pott <alex.a.pott@googlemail.com>
2018-10-09 13:03:56 +0100

    Another way around

---
+++ thunder-develop/scripts/composer/ScriptHandler.php
@@ -0,0 +1,171 @@
+<?php
+/**
+ * @file
+ * Contains \DrupalProject\composer\ScriptHandler.
+ */
+namespace ThunderDevelop\composer;
+
+use Composer\Composer;
+use Composer\Package\PackageInterface;
+use Composer\Script\Event;
+
+use DrupalFinder\DrupalFinder;
+use Symfony\Component\Filesystem\Filesystem;
+use Webmozart\PathUtil\Path;
+
+class ScriptHandler {
+
+  public static function createRequiredFiles(Event $event) {
+    $fs = new Filesystem();
+    $drupalFinder = new DrupalFinder();
+    $drupalFinder->locateRoot(getcwd());
+    $drupalRoot = $drupalFinder->getDrupalRoot();
+    $dirs = [
+      'modules',
+      'profiles',
+      'themes',
+    ];
+    // Required for unit testing
+    foreach ($dirs as $dir) {
+      if (!$fs->exists($drupalRoot . '/' . $dir)) {
+        $fs->mkdir($drupalRoot . '/' . $dir);
+        $fs->touch($drupalRoot . '/' . $dir . '/.gitkeep');
+      }
+    }
+    // Prepare the settings file for installation
+    if (!$fs->exists($drupalRoot . '/sites/default/settings.php') and $fs->exists($drupalRoot . '/sites/default/default.settings.php')) {
+      $fs->copy($drupalRoot . '/sites/default/default.settings.php', $drupalRoot . '/sites/default/settings.php');
+      require_once $drupalRoot . '/core/includes/bootstrap.inc';
+      require_once $drupalRoot . '/core/includes/install.inc';
+      $settings['config_directories'] = [
+        CONFIG_SYNC_DIRECTORY => (object) [
+          'value' => Path::makeRelative($drupalFinder->getComposerRoot() . '/config/sync', $drupalRoot),
+          'required' => TRUE,
+        ],
+      ];
+      drupal_rewrite_settings($settings, $drupalRoot . '/sites/default/settings.php');
+      $fs->chmod($drupalRoot . '/sites/default/settings.php', 0666);
+      $event->getIO()
+        ->write("Create a sites/default/settings.php file with chmod 0666");
+    }
+    // Create the files directory with chmod 0777
+    if (!$fs->exists($drupalRoot . '/sites/default/files')) {
+      $oldmask = umask(0);
+      $fs->mkdir($drupalRoot . '/sites/default/files', 0777);
+      umask($oldmask);
+      $event->getIO()
+        ->write("Create a sites/default/files directory with chmod 0777");
+    }
+  }
+
+  public static function downloadDevelopPackages(Event $event) {
+    $fs = new Filesystem();
+
+    $io = $event->getIO();
+    $composer = $event->getComposer();
+    $repositoryManager = $composer->getRepositoryManager();
+    $rootPackage = $composer->getPackage();
+
+    $rootExtra = $rootPackage->getExtra();
+    $packages = $rootExtra['local-develop-packages'];
+
+    foreach ($packages as $packageString => $packageVersion) {
+      $package = $repositoryManager->findPackage($packageString, $packageVersion);
+      if ($package) {
+        $installPath = self::getInstallPath($package, $composer);
+
+        if (!$fs->exists($installPath)) {
+          $repository = $package->getRepository();
+          if ($gitDriver = $repository->getDriver()) {
+            $gitDriver = $repository->getDriver();
+            $repositoryUrl = $gitDriver->getUrl();
+            $branchOption = (0 === strpos($packageVersion, 'dev-')) ? '-b ' . substr($packageVersion, strlen('dev-')) . ' ' : '';
+            exec('git clone  ' . $branchOption . $repositoryUrl . ' ' . $installPath);
+            $io->write('Cloning repository: ' . $packageString);
+          }
+        }
+      }
+    }
+  }
+
+  /**
+   * Return the install path based on package type.
+   *
+   * @param \Composer\Package\PackageInterface $package
+   * @param \Composer\Composer $composer
+   *
+   * @return bool|string
+   */
+  protected static function getInstallPath(PackageInterface $package, Composer $composer) {
+    $type = $package->getType();
+
+    $prettyName = $package->getPrettyName();
+    if (strpos($prettyName, '/') !== false) {
+      list($vendor, $name) = explode('/', $prettyName);
+    } else {
+      $vendor = '';
+      $name = $prettyName;
+    }
+
+    $availableVars = compact('name', 'vendor', 'type');
+
+    $extra = $package->getExtra();
+    if (!empty($extra['installer-name'])) {
+      $availableVars['name'] = $extra['installer-name'];
+    }
+
+    if ($composer->getPackage()) {
+      $extra = $composer->getPackage()->getExtra();
+      if (!empty($extra['installer-paths'])) {
+        $customPath = self::mapCustomInstallPaths($extra['installer-paths'], $prettyName, $type, $vendor);
+        if ($customPath !== false) {
+          return self::templatePath($customPath, $availableVars);
+        }
+      }
+    }
+
+    return false;
+  }
+
+
+  /**
+   * Search through a passed paths array for a custom install path.
+   *
+   * @param  array  $paths
+   * @param  string $name
+   * @param  string $type
+   * @param  string $vendor = NULL
+   * @return string
+   */
+  protected static function mapCustomInstallPaths(array $paths, $name, $type, $vendor = NULL) {
+    foreach ($paths as $path => $names) {
+      if (in_array($name, $names) || in_array('type:' . $type, $names) || in_array('vendor:' . $vendor, $names)) {
+        return $path;
+      }
+    }
+
+    return false;
+  }
+
+  /**
+   * Replace vars in a path.
+   *
+   * @param  string $path
+   * @param  array  $vars
+   * @return string
+   */
+  protected static function templatePath($path, array $vars = array()) {
+    if (strpos($path, '{') !== false) {
+      extract($vars);
+      preg_match_all('@\{\$([A-Za-z0-9_]*)\}@i', $path, $matches);
+      if (!empty($matches[1])) {
+        foreach ($matches[1] as $var) {
+          $path = str_replace('{$' . $var . '}', $$var, $path);
+        }
+      }
+    }
+
+    return $path;
+  }
+
+}
